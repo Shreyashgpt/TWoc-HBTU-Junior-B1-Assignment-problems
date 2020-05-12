6f819cc2dc645eb08be260e0694f52cf55e64952
Shreyash Gupta <65161647+Shreyashgpt@users.noreply.github.com>
2020-05-12 09:06:03 -0700

    Delete runs.py

--- runs.py
+++
@@ -1,6 +0,0 @@
-a=int(input("Enter runs scored by 1st batsman : "))
-b=int(input("Enter runs scored by 2nd batsman : "))
-c=int(input("Enter runs scored by 3rd batsman : "))
-print("Strike rate of 1st batsman : ", a/60*100,"\nRun scored if they played 60 balls more : ", a*2,"\nMax no.of sixes they could have hit : ", a//6)
-print("Strike rate of 2nd batsman : ", b/60*100,"\nRun scored if they played 60 balls more : ", b*2,"\nMax no.of sixes they could have hit : ", b//6)
-print("Strike rate of 3rd batsman : ", c/60*100,"\nRun scored if they played 60 balls more : ", c*2,"\nMax no.of sixes they could have hit : ", c//6)
