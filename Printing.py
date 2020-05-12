c314b87f41a452dbfbcc162400c7a420248c92a1
Shreyash Gupta <65161647+Shreyashgpt@users.noreply.github.com>
2020-05-12 09:05:18 -0700

    Delete Printing.py

--- Printing.py
+++
@@ -1,6 +0,0 @@
-g=input("Enter Gender  : ")
-b=input("Enter Branch  : ")
-n=input("Enter Name    : ")
-c=input("Enter College : ")
-a=input("Enter Age     : ")
-print("Name    : "+n+ "\nAge     : "+a+ "\nGender  : "+g+ "\nCollege : "+b+ "\nBranch   : "+c)
